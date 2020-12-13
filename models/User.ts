0718677c914ec8ff634a142ea2e2de0224a126c4
Hazem Krimi <krimihazem1@gmail.com>
2020-12-13 21:01:07 +0100

    Add user model

---
+++ models/User.ts
@@ -0,0 +1,28 @@
+import { prop, getModelForClass } from '@typegoose/typegoose';
+import { ObjectType, Field, ID } from 'type-graphql';
+import { File } from './File';
+
+@ObjectType({ description: 'User Model' })
+export class User {
+
+    @Field(() => ID)
+    public id?: String;
+
+    @Field()
+    @prop({ required: true })
+    public username?: string;
+
+    @Field()
+    @prop({ required: true })
+    public email?: string;
+
+    @prop({ required: true })
+    public password?: string;
+
+    @Field(() => [File])
+    @prop({ ref: File, required: false })
+    public files?: File[];
+
+};
+
+export const UserModel = getModelForClass(User);
