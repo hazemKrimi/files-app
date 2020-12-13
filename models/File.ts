315f4aae47f0e711bed58926f7469300c59b4c86
Hazem Krimi <krimihazem1@gmail.com>
2020-12-13 21:02:38 +0100

    Add file model

---
+++ models/File.ts
@@ -0,0 +1,20 @@
+import { prop, getModelForClass } from '@typegoose/typegoose';
+import { ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType({ description: 'File Model' })
+export class File {
+
+    @Field(() => ID)
+    public id?: String;
+
+    @Field()
+    @prop({ required: true })
+    public name?: string;
+
+    @Field()
+    @prop({ required: true })
+    public path?: string;
+
+};
+
+export const FileModel = getModelForClass(File);
